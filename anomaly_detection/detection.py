2023f82c4796c71d4fbb24de9d62a58bf1c6c7b5
Tyler Denton <tdenton8772@gmail.com>
2025-03-19 18:14:59 -0400

    Finished detection script for LLM.

--- anomaly_detection/detection.py
+++ anomaly_detection/detection.py
@@ -2,7 +2,7 @@ import json
 import requests
 import os
 import boto3
-from langchain.embeddings import BedrockEmbeddings
+from langchain_aws.embeddings import BedrockEmbeddings
 
 PINOT_BROKER = os.getenv("PINOT_BROKER")
 PINOT_API_KEY = os.getenv("PINOT_API_KEY")
@@ -11,7 +11,7 @@ PINOT_QUERY_URL = f"{PINOT_BROKER}/query/sql"
 session = boto3.Session(profile_name="default")
 bedrock_client = session.client(service_name='bedrock-runtime', 
                               region_name='us-east-1')
-bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",
+bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0",
                                        client=bedrock_client)
 
 def main():
@@ -41,31 +41,76 @@ def main():
             # Execute each generated query
             for param_set in param_combinations:
                 query_sql = query_template
+                
                 for key, value in param_set.items():
                     query_sql = query_sql.replace(f"%{key}%", value)
-
+                
                 headers = {
                     "Content-Type": "application/json",
                     "authorization": f"Bearer {PINOT_API_KEY}" 
                 }
                 response = requests.post(PINOT_QUERY_URL, json={"sql": query_sql, "queryOptions": "useMultistageEngine=true"}, headers=headers)
-
                 if response.status_code == 200:
                     raw_response = response.json()
                     resultsTable = raw_response['resultTable']
                     column_names = resultsTable.get("dataSchema", {}).get("columnNames", [])
                     rows = resultsTable.get("rows", [])
                     if len(rows) > 0:
-                        alerts.append("{narrative}: {value}")
+                        alerts.append(f"{narrative}: {value}")
                 else:
                     json.dumps({"error": "Failed to fetch data"}), response.status_code
         if len(alerts) > 0:
             text = " ".join(alerts)
             response = bedrock_embeddings.embed_query(text)
-            print(response)
+            sql = f'''SELECT original_text,
+                    l2_distance(vector_data, ARRAY{response}) AS l2_dist
+                    FROM vector_example_v1
+                    WHERE VECTOR_SIMILARITY(vector_data, ARRAY{response}, 3)
+                    order by l2_dist
+                    limit 10
+                    '''
+            headers = {
+                "Content-Type": "application/json",
+                "authorization": f"Bearer {PINOT_API_KEY}"
+            }
+            response = requests.post(PINOT_QUERY_URL, json={"sql": sql}, headers=headers)
+
+            if response.status_code == 200:
+                raw_data = response.json()
+            else:
+                print(json.dumps({"error": "Failed to fetch data"}))
+                exit(1)
+            resultsTable = raw_data['resultTable']
+            rows = resultsTable.get("rows", [])
+            prompt = []
+            prompt.append(f"There was an alert associated with this machine: {text} \n additional information used for generating content is here: \n")
+
+            if len(rows) > 0:
+                for row in rows:
+                    prompt.append(str(row))  # Convert row to a string
+                    prompt.append("use the previous information to generate an alert and recommendation for an HMI")
+
+                final_prompt = "\n".join(prompt)
+                llm_response = bedrock_client.invoke_model(
+                    body=json.dumps({
+                        "anthropic_version": "bedrock-2023-05-31",
+                        "max_tokens": 1000,
+                        "messages": [
+                            {
+                                "role": "user",
+                                "content": final_prompt
+                            }
+                        ]
+                    }),
+                    modelId="us.anthropic.claude-3-7-sonnet-20250219-v1:0"
+                )
+                response_body = json.loads(llm_response["body"].read())
+                assistant_message = response_body["content"][0]['text']
+                print(alerts)
+                print(assistant_message)
+                # need to post this back to the HMI
     except Exception as e:
         print("Execution Error:", str(e))
-        return {"statusCode": 500, "body": str(e)}
 
 if __name__ == "__main__":
     main()
